e9afb7def245795f6bacbcc2bce0ba53bf25886d
EMNayon <emranmasudnayon.017@gmail.com>
2022-02-05 11:44:59 +0600

    first commit

---
+++ api/script.js
@@ -0,0 +1,7 @@
+function display(element){
+    document.getElementById('image').innerHTML = element.alt;
+}
+
+function revert(){
+    document.getElementById('image').innerHTML = "Hover over a sunsign image to display details.";
+}
